1003cdb419b2971f3358dcf9c055998fd04200ef
Stefan Marr <git@stefan-marr.de>
2016-01-11 21:56:54 +0100

    Cached the object factory in the new node
    Object factories are stored in the class, so that we always have only one per classes. Should cache them here so that we do not need to look it up in the class (for which we need to lookup the property first etc...)
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/primitives/basics/NewObjectPrim.java
+++ src/som/primitives/basics/NewObjectPrim.java
@@ -1,19 +1,31 @@
 package som.primitives.basics;
 
+import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
+import com.oracle.truffle.api.dsl.ImportStatic;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.object.DynamicObject;
+import com.oracle.truffle.api.object.DynamicObjectFactory;
 
 import bd.primitives.Primitive;
 import som.interpreter.nodes.nary.UnaryExpressionNode;
-import som.vm.Universe;
+import som.vmobjects.SClass;
+import som.vmobjects.SObject;
 
 
 @GenerateNodeFactory
+@ImportStatic(SClass.class)
 @Primitive(className = "Class", primitive = "new")
 public abstract class NewObjectPrim extends UnaryExpressionNode {
-  @Specialization
-  public final DynamicObject doSClass(final DynamicObject receiver) {
-    return Universe.newInstance(receiver);
+  @Specialization(guards = "receiver == cachedClass")
+  public final DynamicObject cachedClass(final DynamicObject receiver,
+      @Cached("receiver") final DynamicObject cachedClass,
+      @Cached("getFactory(cachedClass)") final DynamicObjectFactory factory) {
+    return factory.newInstance();
+  }
+
+  @Specialization(replaces = "cachedClass")
+  public final DynamicObject uncached(final DynamicObject receiver) {
+    return SObject.create(receiver);
   }
 }
