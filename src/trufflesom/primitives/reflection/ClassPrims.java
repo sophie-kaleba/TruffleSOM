638fe330bca1da4d30a63df2cc660aa644ac3179
Sophie Kaleba <sophie.kaleba@gmail.com>
2021-02-17 16:23:46 +0000

    add a slow primitive to clean the AST

--- src/trufflesom/primitives/reflection/ClassPrims.java
+++ src/trufflesom/primitives/reflection/ClassPrims.java
@@ -4,10 +4,16 @@ import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
 
 import bd.primitives.Primitive;
+import com.oracle.truffle.api.nodes.NodeUtil;
+import com.oracle.truffle.api.nodes.RootNode;
+import trufflesom.interpreter.nodes.dispatch.CachedDispatchNode;
+import trufflesom.interpreter.nodes.nary.BinaryExpressionNode;
 import trufflesom.interpreter.nodes.nary.UnaryExpressionNode;
 import trufflesom.vmobjects.SAbstractObject;
 import trufflesom.vmobjects.SArray;
 import trufflesom.vmobjects.SClass;
+
+import java.util.List;
 
 
 public class ClassPrims {
@@ -46,5 +52,28 @@ public class ClassPrims {
     public final SArray doSClass(final SClass receiver) {
       return receiver.getInstanceFields();
     }
+  }
+
+  @GenerateNodeFactory
+  @Primitive(className = "Class", primitive = "cleanDispatchChain:")
+  public abstract static class CleanDispatchChainPrim extends BinaryExpressionNode {
+    @Specialization
+    public final SAbstractObject doSClass(final SClass receiver, final boolean enabled) {
+
+      if (enabled) {
+        RootNode r = receiver.getInstanceInvokable(1).getCallTarget().getRootNode();
+        List<CachedDispatchNode> allCached = NodeUtil.findAllNodeInstances(r, CachedDispatchNode.class);
+
+        for (int i = 0; i < allCached.size(); i++) {
+          CachedDispatchNode n = allCached.get(i);
+          if (n.getParent().toString().equals("GMsgSend(x)")) {
+            n.replace(allCached.get(i + 3));
+            return receiver;
+          }
+        }
+      }
+      return receiver;
+    }
+
   }
 }
